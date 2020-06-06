e4e0e6bbae03ff5d829d7664c060044aae607bc3
UserBulba <foxxxiu@gmail.com>
2020-06-06 14:18:40 +0200

    Big like Mac update ;)
    Big like Mac update ;)

--- Projects/FortiGenerator/FortiGen.py
+++ Projects/FortiGenerator/FortiGen.py
@@ -1,112 +1,88 @@
 import os
+import sys
 import pandas as pd
-
-path = os.getcwd()
-
 
 class FortiGen():
     
-    sort = []
+    Sort = []
     
-    def __init__(self,host,wan,lan,gateway):
-        self.host = host
-        self.wan = wan
-        self.lan = lan
-        self.gateway = gateway
-        FortiGen.sort.append(self)
-        self.timezone = '29'
+    def __init__(self, Host, Wan, Lan, Gateway, DHCPStart=2, DHCPEnd=100):
+        self.Host = Host
+        self.Wan = Wan
+        self.Lan = Lan
+        self.DHCPStart = DHCPStart
+        self.DHCPEnd = DHCPEnd
+        self.Gateway = Gateway
+        self.TimeZone = '29'
+        self.Path = os.getcwd()
+        self.Sys = os.path.join(self.Path, self.Host)
 
-        
-    def Show(self):
-        print('Host:    {}\n'.format(self.host).upper())
-        print('Wan:     {}'.format(self.wan))
-        print('Lan:     {}'.format(self.lan))
-        print('Gateway: {}'.format(self.gateway))
-        print(FortiGen.sort)
-        return 
+        FortiGen.Sort.append(self)
 
-
-
-    def CreateHostName(self):
+    def FileVerify(self):
+        Error = 0
         try:
-            os.makedirs(os.path.join(path,self.host))
+            if not os.path.exists(os.path.join(self.Path, self.Host)):
+                os.makedirs(os.path.join(self.Path, self.Host))
+            else:
+                print ("Catalogue for device {} found".format(self.Host))
+                Error =+ 1
         except:
-            pass
-        sys = os.path.join(path,self.host)
-        splitIP = self.lan.split(' ')
-        ip_v3 = (splitIP[0]).split('.')
-        dhcp_start = (ip_v3[0] + '.'+ip_v3[1] + '.'+ip_v3[2] + '.2')
-        dhcp_end = (ip_v3[0] + '.'+ip_v3[1] + '.'+ip_v3[2] + '.100')
+            print ("Sorry, error occurred :",sys.exc_info()[0])
+            #Count errors, to continue in loop
+            Error =+ 1
+            
+        return Error
 
-        with open(os.path.join(path,'FortiConfig','DHCP.txt')) as f:
-            newText=f.read().replace('FW502R5618001244',self.host).replace('set timezone 04','set timezone '+self.timezone).replace('set ip 10.10.20.1 255.255.255.0','set ip '+self.lan).replace('set default-gateway 10.10.20.1','set default-gateway ' + splitIP[0]).replace('set start-ip 10.10.20.2','set start-ip '+dhcp_start).replace('set end-ip 10.10.20.254','set end-ip ' + dhcp_end).replace('set ip 91.226.50.102 255.255.248.0', 'set ip '+ self.wan).replace('set gateway 91.226.50.97', 'set gateway ' + self.gateway)
+    def Replace(self):
 
+        Subnet = self.Lan.split(' ')
+        LanIP = (Subnet[0]).split('.')
 
-        with open(os.path.join(sys,'fgt_config.conf'), "w") as f:
-            f.write(newText)       
+        DHCPStartAddress = (LanIP[0] + '.'+ LanIP[1] + '.' + LanIP[2] + str(self.DHCPStart))
+        DHCPEndAddress = (LanIP[0] + '.'+ LanIP[1] + '.' + LanIP[2] + str(self.DHCPEnd))
+
+        with open(os.path.join(self.Path,'FortiConfig','DHCP.txt')) as Config:
+            newText = Config.read().replace('FW502R5618001244',self.Host)
+            newText = newText.replace('set timezone 04','set timezone ' + self.TimeZone)
+            newText = newText.replace('set ip 10.10.20.1 255.255.255.0','set ip ' + self.Lan)
+            newText = newText.replace('set default-gateway 10.10.20.1','set default-gateway ' + Subnet[0])
+            newText = newText.replace('set start-ip 10.10.20.2','set start-ip ' + DHCPStartAddress)
+            newText = newText.replace('set end-ip 10.10.20.254','set end-ip ' + DHCPEndAddress)
+            newText = newText.replace('set ip 91.226.50.102 255.255.248.0', 'set ip ' + self.Wan)
+            newText = newText.replace('set gateway 91.226.50.97', 'set gateway ' + self.Gateway)
+
+        with open(os.path.join(self.Sys,'fgt_config.conf'), "w") as Config:
+            Config.write(newText)       
+
         return
 
+    def Main(self):
+        if self.Wan == 'DHCP':
+            CountError = self.FileVerify()
+            
+            
+            print ('DHCP configuration :', self.Wan, "-"*3, self.Host)
 
-    def modeValidation(self):
-        print(self.host)
-        if self.wan == 'DHCP':
-            self.CreateHostName()
+            if CountError:
+                print (CountError)
+
+            self.Replace()
+
         else:
-            print('No File')
-        
-        return  
+            CountError = self.FileVerify()
 
+            print('Static configuration :', self.Wan, "-"*3, self.Host)
 
-excel = pd.read_csv(os.path.join(path,'FortiConfig','Basic.csv'),usecols=['Host','WAN','LAN','Gateway'])
+            if CountError:
+                print (CountError)
 
+        return CountError
 
-s = 0
+excel = pd.read_csv(os.path.join(os.getcwd(),'FortiConfig','Basic.csv'),usecols=['Host','WAN','LAN','Gateway'])
+
 for i in range(len(excel)):
     FortiGen((excel['Host'][i]),(excel['WAN'][i]),(excel['LAN'][i]),(excel['Gateway'][i]))
     
-    FortiGen.modeValidation(FortiGen.sort[i])
+    FortiGen.Main(FortiGen.Sort[i])
     
-    
-
-'''
-        dhcp_txt = open(os.path.join(path,'FortiConfig','DHCP.txt'),'rt')
-
-        os.makedirs(os.path.join(path,self.host))
-        sys = os.path.join(path,self.host)
-        dhcp_output = open(os.path.join(sys,'fgt_config.conf'),'w+')
-
-
-
-        for line in dhcp_txt:
-           dhcp_output.write(line.replace('FW502R5618001244',self.host))
-        
-
-        dhcp_output.close()
-        dhcp_txt.close()
-'''
-        
-
-    
-
-
-
-
-
-
-#for p in FortiGen.sort:
-#    FortiGen.Show(p)
-
-'''
-my_list = []
-
-for i in range((excel.shape[0])):
-    my_list.append(list(excel.iloc[i, :]))
-
-print(my_list[0][0])
-s = 0
-for p in my_list:
-    FortiGen((my_list[s][s]),(my_list[s][s+1]),(my_list[s][s+2]),(my_list[s][s+3]))
-
-for t in FortiGen.sort:
-    FortiGen.Show(t)
-'''
