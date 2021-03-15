881148e5874e013001af9f0bc252246259a35088
UserBulba <foxxxiu@gmail.com>
2021-03-16 00:24:33 +0100

    Cleanup

--- Learning/Var&Code#2.py
+++
@@ -1,6 +0,0 @@
-projects = ['Brexit', 'Nord Stream', 'US Mexico Border']
-leaders = ['Theresa May', 'Wladimir Putin', 'Donald Trump and Bill Clinton']
-dates = ['2016-06-23', '2016-08-29', '1994-01-01']
-
-for position, (project, leader, date) in enumerate(zip(projects,leaders,dates)):
-    print ("{} - The leader of project {} started {} is {}".format(position+1,project,date,leader))
