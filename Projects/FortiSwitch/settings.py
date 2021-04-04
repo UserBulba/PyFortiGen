e0527d26e76430259ec5073979c0ab744ef953d5
UserPiotr <60006356+UserPiotr@users.noreply.github.com>
2021-04-04 02:46:16 +0200

    Create settings.py

---
+++ Projects/FortiSwitch/settings.py
@@ -0,0 +1,14 @@
+'''Settings file'''
+# settings.py
+
+# Variables definition
+
+# SNMP Community
+HOSTS = ["10.70.8.173/255.255.255.255"]
+INTERFACE = "internal"
+COMMUNITY = "OtCfCommPuB"
+
+# SysInfo
+LOCATION = "OTCF"
+DESCRIPTION = "FortiSwitch device, property of OTCF company."
+CONTACT = "admin@otcf.pl"
