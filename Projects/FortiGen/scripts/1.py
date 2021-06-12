32fcb0728c89924ba70d1e58bde0ebca030a295a
UserBulba <foxxxiu@gmail.com>
2021-06-12 18:00:59 +0200

    Delete 1.py

--- Projects/FortiGen/scripts/1.py
+++
@@ -1,6 +0,0 @@
-from ipaddress import IPv4Interface, IPv4Network
-device = "10.149.160.1"
-ifc = IPv4Interface(device + "/28")
-
-print(ifc.ip)  # The host IP address
-print(ifc.network)  # Network in which the host IP resides
