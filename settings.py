e339fd468fd2f233e9dd559f3c3b545b4ebf06ca
UserBulba <foxxxiu@gmail.com>
2021-09-08 16:35:58 +0200

    Static golden image hidden.

--- settings.py
+++ settings.py
@@ -1,8 +1,6 @@
 '''Settings file'''
 # settings.py
 
-import os
-
 # Network prefix to validate.
 PREFIX = "10.128.0.0/9"
 
@@ -16,16 +14,19 @@ AGGREGATED_MASK = "28"
 PATTERN = (r'\b([R]{1})(-[A-Z]{2})([0-9]{5})\b')
 
 # Golden image file name in conf directory.
-GOLDEN_IMAGE_FILE = "golden_config_fgt_system.conf"
+# GOLDEN_IMAGE_FILE = "golden_config_fgt_system.conf"
 
 # Output config file name.
 CONFIG_FILE = "fgt_system.conf"
 
-# Create path to golden image base on realpath location.
-current_dir = (os.path.dirname(os.path.realpath(__file__)))
-golden_image_path = os.path.join(current_dir, "conf", GOLDEN_IMAGE_FILE)
+# FortiGate platform.
+PLATFORM = ["40F", "50E"]
 
-GOLDEN_IMAGE_PATH = golden_image_path
+# Create path to golden image base on realpath location.
+# current_dir = (os.path.dirname(os.path.realpath(__file__)))
+# golden_image_path = os.path.join(current_dir, "conf", GOLDEN_IMAGE_FILE)
+
+# GOLDEN_IMAGE_PATH = golden_image_path
 
 # Values in config file to replace.
 DEVICE_NAME = "R-DUMMY"
@@ -36,3 +37,7 @@ FORTILINK_DHCP_START = "FORTILINK_DHCP_START"
 FORTILINK_DHCP_STOP = "FORTILINK_DHCP_STOP"
 FORTILINK_DHCP_START_VALUE = 2
 FORTILINK_DHCP_STOP_VALUE = 254
+
+# GUI settings
+COLOR = "white"
+MESSAGE = "Choose the FortiGate platform"
